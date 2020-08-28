4a61d17c4d1e0782118fc2951eb5bb1798f23692
prithvirajv <prithviraj.varla@concur.com>
2020-08-28 10:48:04 -0500

    checkout debug 14

--- src/components/MenuComponents/checkout/Components/index.js
+++ src/components/MenuComponents/checkout/Components/index.js
@@ -22,10 +22,53 @@ render() {
     } = this.props;
         
   return (
-
-    //<Form onSubmit={handleSubmit}>
       
     <Form onSubmit={handleSubmit(this.props.actionSubmit)}>
+
+        <FormGroup>
+            <Field name="firstName" type="firstName" id="firstName"
+                htmlFor="firstName"
+                component={renderInputField} label="First Name"
+                validate={[ required, minValue18 ]}
+                className="contact-input"
+            />
+        </FormGroup>
+
+        <FormGroup>
+            <Field name="lastName" type="lastName" id="lastName"
+                htmlFor="lastName"
+                component={renderInputField} label="Last Name"
+                validate={[ required, minValue18 ]}
+                className="contact-input"
+            />
+        </FormGroup>
+
+        <FormGroup>
+            <Field name="emailAddress" type="email" id="email"
+                htmlFor="email"
+                component={renderInputField} label="Email"
+                validate={[ required, email ]}
+                warn={aol}
+                className="contact-input"
+            />
+        </FormGroup>
+
+        <FormGroup>
+            <Field name="phonenumber" type="text" id="phonenumber"
+                htmlFor="phonenumber"
+                component={renderInputField} label="Phone Number"
+                validate={[ required, minValue18 ]}
+                className="contact-input"
+            />
+        </FormGroup>
+        
+
+        <FormGroup>
+        <Label htmlFor="exampleText">Comments</Label>
+        <Input className="deliveryInstructions"
+        placeholder= "Delivery instructions (e.g. Check in with doorman.)" 
+        type="textarea" name="text" id="exampleText" />
+        </FormGroup>
 
         <button color="primary" className='continue-to-payment'  type="submit" disabled={submitting}>
             Continue to payment method
